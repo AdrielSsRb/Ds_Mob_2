ccf10280178249d84ec8500b66bcfcb49c6b3979
Desenvolvimento <dsadm@educ501.local>
2024-02-08 22:44:33 -0300

    Criado 2 aplicacoes da aula 2

---
+++ aplicacao2_aula2/android/app/src/main/kotlin/com/example/aplicacao2_aula2/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.aplicacao2_aula2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ aplicacao_aula2/android/app/src/main/kotlin/com/example/aplicacao_aula2/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.aplicacao_aula2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
