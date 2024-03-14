19475d42e0cdd15f79447557a5c4d87b665cedf0
Plinio1065976 <125322247+Plinio1065976@users.noreply.github.com>
2024-03-14 19:40:55 -0300

    Feito a aplicação da aula 07

---
+++ aula_07/aplicacao_aula07/android/app/src/main/kotlin/mobile/example/aplicacao_aula07/MainActivity.kt
@@ -0,0 +1,6 @@
+package mobile.example.aplicacao_aula07
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ aula_07/aplicacao_aula07/lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:aplicacao_aula07/Home.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Home(),
+  ));
+}
